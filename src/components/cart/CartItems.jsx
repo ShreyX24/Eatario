93d7f50ad84d08e3c14af8f96089d0faab388fa5
DREADEYE <134378619+DREADEYE-XD@users.noreply.github.com>
2024-09-28 16:25:54 +0530

    updated

--- src/components/cart/CartItems.jsx
+++ src/components/cart/CartItems.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CartItems = ({item, onRemove}) => {
+const CartItems = ({ item, onRemove }) => {
   return (
     <div className="flex w-full h-[120px] items-center  text-red-500 font-semibold md:justify-end">
       <img
@@ -9,13 +9,15 @@ const CartItems = ({item, onRemove}) => {
         className="object-contain h-[90%] w-1/3 md:w-auto"
       />
       <div className="flex justify-start items-center w-[65%] gap-16 md:justify-end md:w-auto md:px-10">
-        <div className="flex flex-col w-[240px] overflow-hidden">
-          <span className="font-bold text-2xl uppercase ">{item.title}</span>
+        <div className=" flex-col hidden md:flex md:w-[350px] overflow-hidden">
+          <span className=" font-bold text-2xl uppercase ">{item.title}</span>
           <span>Size: {item.size}</span>
         </div>
-        <span className="font-bold">${item.totalPrice}</span>
+        <span className="font-bold">${item.totalPrice.toFixed(2)}</span>
         <span className="font-bold">{item.quantity}</span>
-        <button onClick={onRemove} className="">X</button>
+        <button onClick={onRemove} className="">
+          X
+        </button>
       </div>
     </div>
   );
